879b66a7a8e89495d354997e8bd5bd7f1213a0a2
Eugene Russkikh <zitryss@gmail.com>
2020-07-10 20:23:53 +0200

    Fix CI

--- infrastructure/storage/mock.go
+++ infrastructure/storage/mock.go
@@ -11,16 +11,16 @@ func NewMock() mock {
 type mock struct {
 }
 
-func (m *mock) Put(_ context.Context, album string, image string, b []byte) (string, error) {
+func (m *mock) Put(_ context.Context, album string, image string, _ []byte) (string, error) {
 	filename := "albums/" + album + "/images/" + image
 	src := "/aye-and-nay/" + filename
 	return src, nil
 }
 
-func (m *mock) Get(_ context.Context, album string, image string) ([]byte, error) {
+func (m *mock) Get(_ context.Context, _ string, _ string) ([]byte, error) {
 	return nil, nil
 }
 
-func (m *mock) Remove(_ context.Context, album string, image string) error {
+func (m *mock) Remove(_ context.Context, _ string, _ string) error {
 	return nil
 }

4a53b08f250bda1f6ee64566a7f7c040177c9c3f
Eugene Russkikh <zitryss@gmail.com>
2020-07-10 19:43:05 +0200

    Optimize memory use

--- infrastructure/storage/mock.go
+++ infrastructure/storage/mock.go
@@ -2,41 +2,23 @@ package storage
 
 import (
 	"context"
-	"sync"
 )
 
 func NewMock() mock {
-	return mock{
-		files: map[string][]byte{},
-	}
+	return mock{}
 }
 
 type mock struct {
-	sync.Mutex
-	files map[string][]byte
 }
 
 func (m *mock) Put(_ context.Context, album string, image string, b []byte) (string, error) {
-	m.Lock()
-	defer m.Unlock()
-	filename := "albums/" + album + "/images/" + image
-	m.files[filename] = b
-	src := "/aye-and-nay/" + filename
-	return src, nil
+	return "", nil
 }
 
 func (m *mock) Get(_ context.Context, album string, image string) ([]byte, error) {
-	m.Lock()
-	defer m.Unlock()
-	filename := "albums/" + album + "/images/" + image
-	b := m.files[filename]
-	return b, nil
+	return nil, nil
 }
 
 func (m *mock) Remove(_ context.Context, album string, image string) error {
-	m.Lock()
-	defer m.Unlock()
-	filename := "albums/" + album + "/images/" + image
-	delete(m.files, filename)
 	return nil
 }

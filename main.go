888b46dd59bd080a7ae4467e88bb8dbe0e8f9662
Eugene Russkikh <zitryss@gmail.com>
2020-07-23 21:41:57 +0200

    Implement ballast

--- main.go
+++ main.go
@@ -18,6 +18,10 @@ import (
 	"github.com/zitryss/aye-and-nay/pkg/log"
 )
 
+var (
+	ballast []byte
+)
+
 func main() {
 	conf := ""
 	flag.StringVar(&conf, "config", ".", "relative path to config file")
@@ -30,6 +34,8 @@ func main() {
 		log.Critical(err)
 		os.Exit(1)
 	}
+
+	ballast = make([]byte, viper.GetInt64("app.ballast"))
 
 	lvl := viper.GetString("app.log")
 	log.SetOutput(os.Stderr)

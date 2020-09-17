3c5c024e78cce3549afff8a1a08474e21df1ce37
Eugene Russkikh <zitryss@gmail.com>
2020-09-17 11:04:07 +0200

    Repair tests

--- delivery/http/controller_integration_test.go
+++ delivery/http/controller_integration_test.go
@@ -208,7 +208,7 @@ func TestControllerIntegrationHandleAlbum(t *testing.T) {
 		fn(w, r, nil)
 		CheckStatusCode(t, w, 413)
 		CheckContentType(t, w, "text/plain; charset=utf-8")
-		CheckBody(t, w, `Request Entity Too Large`+"\n")
+		CheckBody(t, w, `Too Many Images`+"\n")
 	})
 	t.Run("Negative3", func(t *testing.T) {
 		comp := compressor.NewMock()
@@ -255,7 +255,7 @@ func TestControllerIntegrationHandleAlbum(t *testing.T) {
 		fn(w, r, nil)
 		CheckStatusCode(t, w, 413)
 		CheckContentType(t, w, "text/plain; charset=utf-8")
-		CheckBody(t, w, `Request Entity Too Large`+"\n")
+		CheckBody(t, w, `Image Too Large`+"\n")
 	})
 	t.Run("Negative4", func(t *testing.T) {
 		comp := compressor.NewMock()
@@ -302,7 +302,7 @@ func TestControllerIntegrationHandleAlbum(t *testing.T) {
 		fn(w, r, nil)
 		CheckStatusCode(t, w, 415)
 		CheckContentType(t, w, "text/plain; charset=utf-8")
-		CheckBody(t, w, `Unsupported Media Type`+"\n")
+		CheckBody(t, w, `Unsupported Image Format`+"\n")
 	})
 	t.Run("Negative5", func(t *testing.T) {
 		fn1 := func() func(int) (string, error) {

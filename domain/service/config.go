18bb1e3bba80fb1ed7f7e4f264c2d9c99e2bd390
Eugene Russkikh <zitryss@gmail.com>
2021-03-10 16:06:45 +0100

    Update config files respectively

--- domain/service/config.go
+++ domain/service/config.go
@@ -8,16 +8,10 @@ func newServiceConfig() serviceConfig {
 	return serviceConfig{
 		numberOfWorkersCalc: viper.GetInt("service.numberOfWorkersCalc"),
 		numberOfWorkersComp: viper.GetInt("service.numberOfWorkersComp"),
-		albumIdLength:       viper.GetInt("service.albumIdLength"),
-		imageIdLength:       viper.GetInt("service.imageIdLength"),
-		tokenIdLength:       viper.GetInt("service.tokenIdLength"),
 	}
 }
 
 type serviceConfig struct {
 	numberOfWorkersCalc int
 	numberOfWorkersComp int
-	albumIdLength       int
-	imageIdLength       int
-	tokenIdLength       int
 }

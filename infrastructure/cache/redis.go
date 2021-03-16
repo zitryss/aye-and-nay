ddab84c54a292c329904f93f822023b639015537
Eugene Russkikh <zitryss@gmail.com>
2021-03-16 22:40:04 +0100

    Introduce new type to the cache

--- infrastructure/cache/redis.go
+++ infrastructure/cache/redis.go
@@ -8,6 +8,7 @@ import (
 	redisdb "github.com/go-redis/redis/v8"
 
 	"github.com/zitryss/aye-and-nay/domain/model"
+	"github.com/zitryss/aye-and-nay/pkg/base64"
 	"github.com/zitryss/aye-and-nay/pkg/errors"
 	"github.com/zitryss/aye-and-nay/pkg/retry"
 )
@@ -36,20 +37,22 @@ type Redis struct {
 }
 
 func (r *Redis) Add(ctx context.Context, queue uint64, album uint64) error {
-	key1 := "queue:" + queue + ":set"
-	ok, err := r.client.SIsMember(ctx, key1, album).Result()
+	queueB64 := base64.FromUint64(queue)
+	albumB64 := base64.FromUint64(album)
+	key1 := "queue:" + queueB64 + ":set"
+	ok, err := r.client.SIsMember(ctx, key1, albumB64).Result()
 	if err != nil {
 		return errors.Wrap(err)
 	}
 	if ok {
 		return nil
 	}
-	_, err = r.client.SAdd(ctx, key1, album).Result()
+	_, err = r.client.SAdd(ctx, key1, albumB64).Result()
 	if err != nil {
 		return errors.Wrap(err)
 	}
-	key2 := "queue:" + queue + ":list"
-	_, err = r.client.RPush(ctx, key2, album).Result()
+	key2 := "queue:" + queueB64 + ":list"
+	_, err = r.client.RPush(ctx, key2, albumB64).Result()
 	if err != nil {
 		return errors.Wrap(err)
 	}
@@ -57,21 +60,27 @@ func (r *Redis) Add(ctx context.Context, queue uint64, album uint64) error {
 }
 
 func (r *Redis) Poll(ctx context.Context, queue uint64) (uint64, error) {
-	key1 := "queue:" + queue + ":list"
-	album, err := r.client.LPop(ctx, key1).Result()
+	queueB64 := base64.FromUint64(queue)
+	key1 := "queue:" + queueB64 + ":list"
+	albumB64, err := r.client.LPop(ctx, key1).Result()
 	if errors.Is(err, redisdb.Nil) {
-		return "", errors.Wrap(model.ErrUnknown)
+		return 0x0, errors.Wrap(model.ErrUnknown)
 	}
-	key2 := "queue:" + queue + ":set"
-	_, err = r.client.SRem(ctx, key2, album).Result()
+	key2 := "queue:" + queueB64 + ":set"
+	_, err = r.client.SRem(ctx, key2, albumB64).Result()
 	if err != nil {
-		return "", errors.Wrap(err)
+		return 0x0, errors.Wrap(err)
+	}
+	album, err := base64.ToUint64(albumB64)
+	if err != nil {
+		return 0x0, errors.Wrap(err)
 	}
 	return album, nil
 }
 
 func (r *Redis) Size(ctx context.Context, queue uint64) (int, error) {
-	key := "queue:" + queue + ":set"
+	queueB64 := base64.FromUint64(queue)
+	key := "queue:" + queueB64 + ":set"
 	n, err := r.client.SCard(ctx, key).Result()
 	if err != nil {
 		return 0, errors.Wrap(err)
@@ -80,8 +89,10 @@ func (r *Redis) Size(ctx context.Context, queue uint64) (int, error) {
 }
 
 func (r *Redis) PAdd(ctx context.Context, pqueue uint64, album uint64, expires time.Time) error {
-	key := "pqueue:" + pqueue + ":sortedset"
-	err := r.client.ZAdd(ctx, key, &redisdb.Z{Score: float64(expires.UnixNano()), Member: album}).Err()
+	pqueueB64 := base64.FromUint64(pqueue)
+	albumB64 := base64.FromUint64(album)
+	key := "pqueue:" + pqueueB64 + ":sortedset"
+	err := r.client.ZAdd(ctx, key, &redisdb.Z{Score: float64(expires.UnixNano()), Member: albumB64}).Err()
 	if err != nil {
 		return errors.Wrap(err)
 	}
@@ -89,21 +100,26 @@ func (r *Redis) PAdd(ctx context.Context, pqueue uint64, album uint64, expires t
 }
 
 func (r *Redis) PPoll(ctx context.Context, pqueue uint64) (uint64, time.Time, error) {
-	key := "pqueue:" + pqueue + ":sortedset"
+	pqueueB64 := base64.FromUint64(pqueue)
+	key := "pqueue:" + pqueueB64 + ":sortedset"
 	val, err := r.client.ZPopMin(ctx, key).Result()
 	if err != nil {
-		return "", time.Time{}, errors.Wrap(err)
+		return 0x0, time.Time{}, errors.Wrap(err)
 	}
 	if len(val) == 0 {
-		return "", time.Time{}, errors.Wrap(model.ErrUnknown)
+		return 0x0, time.Time{}, errors.Wrap(model.ErrUnknown)
 	}
-	album := val[0].Member.(string)
+	album, err := base64.ToUint64(val[0].Member.(string))
+	if err != nil {
+		return 0x0, time.Time{}, errors.Wrap(err)
+	}
 	expires := time.Unix(0, int64(val[0].Score))
 	return album, expires, nil
 }
 
 func (r *Redis) PSize(ctx context.Context, pqueue uint64) (int, error) {
-	key := "pqueue:" + pqueue + ":sortedset"
+	pqueueB64 := base64.FromUint64(pqueue)
+	key := "pqueue:" + pqueueB64 + ":sortedset"
 	n, err := r.client.ZCard(ctx, key).Result()
 	if err != nil {
 		return 0, errors.Wrap(err)
@@ -112,10 +128,13 @@ func (r *Redis) PSize(ctx context.Context, pqueue uint64) (int, error) {
 }
 
 func (r *Redis) Push(ctx context.Context, album uint64, pairs [][2]uint64) error {
-	key := "album:" + album + ":pairs"
+	albumB64 := base64.FromUint64(album)
+	key := "album:" + albumB64 + ":pairs"
 	pipe := r.client.Pipeline()
 	for _, images := range pairs {
-		pipe.RPush(ctx, key, images[0]+":"+images[1])
+		image0B64 := base64.FromUint64(images[0])
+		image1B64 := base64.FromUint64(images[1])
+		pipe.RPush(ctx, key, image0B64+":"+image1B64)
 	}
 	pipe.Expire(ctx, key, r.conf.timeToLive)
 	_, err := pipe.Exec(ctx)
@@ -126,27 +145,39 @@ func (r *Redis) Push(ctx context.Context, album uint64, pairs [][2]uint64) error
 }
 
 func (r *Redis) Pop(ctx context.Context, album uint64) (uint64, uint64, error) {
-	key := "album:" + album + ":pairs"
+	albumB64 := base64.FromUint64(album)
+	key := "album:" + albumB64 + ":pairs"
 	n, err := r.client.LLen(ctx, key).Result()
 	if err != nil {
-		return "", "", errors.Wrap(err)
+		return 0x0, 0x0, errors.Wrap(err)
 	}
 	if n == 0 {
-		return "", "", errors.Wrap(model.ErrPairNotFound)
+		return 0x0, 0x0, errors.Wrap(model.ErrPairNotFound)
 	}
 	val, err := r.client.LPop(ctx, key).Result()
 	if err != nil {
-		return "", "", errors.Wrap(err)
+		return 0x0, 0x0, errors.Wrap(err)
 	}
-	images := strings.Split(val, ":")
-	if len(images) != 2 {
-		return "", "", errors.Wrap(model.ErrUnknown)
+	imagesB64 := strings.Split(val, ":")
+	if len(imagesB64) != 2 {
+		return 0x0, 0x0, errors.Wrap(model.ErrUnknown)
 	}
-	return images[0], images[1], nil
+	image0, err := base64.ToUint64(imagesB64[0])
+	if err != nil {
+		return 0x0, 0x0, errors.Wrap(err)
+	}
+	image1, err := base64.ToUint64(imagesB64[1])
+	if err != nil {
+		return 0x0, 0x0, errors.Wrap(err)
+	}
+	return image0, image1, nil
 }
 
 func (r *Redis) Set(ctx context.Context, album uint64, token uint64, image uint64) error {
-	key := "album:" + album + ":token:" + token + ":image"
+	albumB64 := base64.FromUint64(album)
+	tokenB64 := base64.FromUint64(token)
+	imageB64 := base64.FromUint64(image)
+	key := "album:" + albumB64 + ":token:" + tokenB64 + ":image"
 	n, err := r.client.Exists(ctx, key).Result()
 	if err != nil {
 		return errors.Wrap(err)
@@ -154,7 +185,7 @@ func (r *Redis) Set(ctx context.Context, album uint64, token uint64, image uint6
 	if n == 1 {
 		return errors.Wrap(model.ErrTokenAlreadyExists)
 	}
-	err = r.client.Set(ctx, key, image, r.conf.timeToLive).Err()
+	err = r.client.Set(ctx, key, imageB64, r.conf.timeToLive).Err()
 	if err != nil {
 		return errors.Wrap(err)
 	}
@@ -162,17 +193,23 @@ func (r *Redis) Set(ctx context.Context, album uint64, token uint64, image uint6
 }
 
 func (r *Redis) Get(ctx context.Context, album uint64, token uint64) (uint64, error) {
-	key := "album:" + album + ":token:" + token + ":image"
-	image, err := r.client.Get(ctx, key).Result()
+	albumB64 := base64.FromUint64(album)
+	tokenB64 := base64.FromUint64(token)
+	key := "album:" + albumB64 + ":token:" + tokenB64 + ":image"
+	imageB64, err := r.client.Get(ctx, key).Result()
 	if errors.Is(err, redisdb.Nil) {
-		return "", errors.Wrap(model.ErrTokenNotFound)
+		return 0x0, errors.Wrap(model.ErrTokenNotFound)
 	}
 	if err != nil {
-		return "", errors.Wrap(err)
+		return 0x0, errors.Wrap(err)
 	}
 	err = r.client.Del(ctx, key).Err()
 	if err != nil {
-		return "", errors.Wrap(err)
+		return 0x0, errors.Wrap(err)
+	}
+	image, err := base64.ToUint64(imageB64)
+	if err != nil {
+		return 0x0, errors.Wrap(err)
 	}
 	return image, nil
 }

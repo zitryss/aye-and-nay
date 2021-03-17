311894c63a41696e043522321cf120e630759344
Eugene Russkikh <zitryss@gmail.com>
2021-03-17 10:31:21 +0100

    Prepare database to the type change

--- infrastructure/database/mem.go
+++ infrastructure/database/mem.go
@@ -13,7 +13,7 @@ func NewMem() *Mem {
 	conf := newMemConfig()
 	return &Mem{
 		conf:       conf,
-		syncAlbums: syncAlbums{albums: map[string]model.Album{}},
+		syncAlbums: syncAlbums{albums: map[uint64]model.Album{}},
 	}
 }
 
@@ -24,7 +24,7 @@ type Mem struct {
 
 type syncAlbums struct {
 	sync.Mutex
-	albums map[string]model.Album
+	albums map[uint64]model.Album
 }
 
 func (m *Mem) SaveAlbum(_ context.Context, alb model.Album) error {
@@ -34,18 +34,18 @@ func (m *Mem) SaveAlbum(_ context.Context, alb model.Album) error {
 	if ok {
 		return errors.Wrap(model.ErrAlbumAlreadyExists)
 	}
-	edgs := make(map[string]map[string]int, len(alb.Images))
+	edgs := make(map[uint64]map[uint64]int, len(alb.Images))
 	for i := range alb.Images {
 		img := &alb.Images[i]
 		img.Compressed = m.conf.compressed
-		edgs[img.Id] = make(map[string]int, len(alb.Images))
+		edgs[img.Id] = make(map[uint64]int, len(alb.Images))
 	}
 	alb.Edges = edgs
 	m.albums[alb.Id] = alb
 	return nil
 }
 
-func (m *Mem) CountImages(ctx context.Context, album uint64) (int, error) {
+func (m *Mem) CountImages(_ context.Context, album uint64) (int, error) {
 	m.syncAlbums.Lock()
 	defer m.syncAlbums.Unlock()
 	alb, ok := m.albums[album]
@@ -56,7 +56,7 @@ func (m *Mem) CountImages(ctx context.Context, album uint64) (int, error) {
 	return n, nil
 }
 
-func (m *Mem) CountImagesCompressed(ctx context.Context, album uint64) (int, error) {
+func (m *Mem) CountImagesCompressed(_ context.Context, album uint64) (int, error) {
 	m.syncAlbums.Lock()
 	defer m.syncAlbums.Unlock()
 	alb, ok := m.albums[album]
@@ -72,7 +72,7 @@ func (m *Mem) CountImagesCompressed(ctx context.Context, album uint64) (int, err
 	return n, nil
 }
 
-func (m *Mem) UpdateCompressionStatus(ctx context.Context, album uint64, image uint64) error {
+func (m *Mem) UpdateCompressionStatus(_ context.Context, album uint64, image uint64) error {
 	m.syncAlbums.Lock()
 	defer m.syncAlbums.Unlock()
 	alb, ok := m.albums[album]
@@ -94,7 +94,7 @@ func (m *Mem) UpdateCompressionStatus(ctx context.Context, album uint64, image u
 	return nil
 }
 
-func (m *Mem) GetImage(ctx context.Context, album uint64, image uint64) (model.Image, error) {
+func (m *Mem) GetImage(_ context.Context, album uint64, image uint64) (model.Image, error) {
 	m.syncAlbums.Lock()
 	defer m.syncAlbums.Unlock()
 	alb, ok := m.albums[album]
@@ -116,21 +116,21 @@ func (m *Mem) GetImage(ctx context.Context, album uint64, image uint64) (model.I
 	return alb.Images[index], nil
 }
 
-func (m *Mem) GetImages(ctx context.Context, album uint64) ([]uint64, error) {
+func (m *Mem) GetImages(_ context.Context, album uint64) ([]uint64, error) {
 	m.syncAlbums.Lock()
 	defer m.syncAlbums.Unlock()
 	alb, ok := m.albums[album]
 	if !ok {
 		return nil, errors.Wrap(model.ErrAlbumNotFound)
 	}
-	images := make([]string, 0, len(alb.Images))
+	images := make([]uint64, 0, len(alb.Images))
 	for _, img := range alb.Images {
 		images = append(images, img.Id)
 	}
 	return images, nil
 }
 
-func (m *Mem) SaveVote(ctx context.Context, album uint64, imageFrom uint64, imageTo uint64) error {
+func (m *Mem) SaveVote(_ context.Context, album uint64, imageFrom uint64, imageTo uint64) error {
 	m.syncAlbums.Lock()
 	defer m.syncAlbums.Unlock()
 	alb, ok := m.albums[album]
@@ -141,16 +141,16 @@ func (m *Mem) SaveVote(ctx context.Context, album uint64, imageFrom uint64, imag
 	return nil
 }
 
-func (m *Mem) GetEdges(ctx context.Context, album uint64) (map[uint64]map[uint64]int, error) {
+func (m *Mem) GetEdges(_ context.Context, album uint64) (map[uint64]map[uint64]int, error) {
 	m.syncAlbums.Lock()
 	defer m.syncAlbums.Unlock()
 	alb, ok := m.albums[album]
 	if !ok {
 		return nil, errors.Wrap(model.ErrAlbumNotFound)
 	}
-	edgs := make(map[string]map[string]int, len(alb.Edges))
+	edgs := make(map[uint64]map[uint64]int, len(alb.Edges))
 	for k := range alb.Edges {
-		edgs[k] = make(map[string]int, len(alb.Edges[k]))
+		edgs[k] = make(map[uint64]int, len(alb.Edges[k]))
 	}
 	for k1 := range alb.Edges {
 		for k2 := range alb.Edges[k1] {
@@ -160,7 +160,7 @@ func (m *Mem) GetEdges(ctx context.Context, album uint64) (map[uint64]map[uint64
 	return edgs, nil
 }
 
-func (m *Mem) UpdateRatings(ctx context.Context, album uint64, vector map[uint64]float64) error {
+func (m *Mem) UpdateRatings(_ context.Context, album uint64, vector map[uint64]float64) error {
 	m.syncAlbums.Lock()
 	defer m.syncAlbums.Unlock()
 	alb, ok := m.albums[album]
@@ -179,7 +179,7 @@ func (m *Mem) UpdateRatings(ctx context.Context, album uint64, vector map[uint64
 	return nil
 }
 
-func (m *Mem) GetImagesOrdered(ctx context.Context, album uint64) ([]model.Image, error) {
+func (m *Mem) GetImagesOrdered(_ context.Context, album uint64) ([]model.Image, error) {
 	m.syncAlbums.Lock()
 	defer m.syncAlbums.Unlock()
 	alb, ok := m.albums[album]
@@ -192,7 +192,7 @@ func (m *Mem) GetImagesOrdered(ctx context.Context, album uint64) ([]model.Image
 	return imgs, nil
 }
 
-func (m *Mem) DeleteAlbum(ctx context.Context, album uint64) error {
+func (m *Mem) DeleteAlbum(_ context.Context, album uint64) error {
 	m.syncAlbums.Lock()
 	defer m.syncAlbums.Unlock()
 	_, ok := m.albums[album]

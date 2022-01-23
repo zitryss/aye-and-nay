3fb5c4fc17ccc4c2f1d70ad0c749d6ef81632692
Eugene Russkikh <z8a7hp@gmail.com>
2022-01-23 01:04:36 +0100

    Stop rerouting requests

--- delivery/http/router.go
+++ delivery/http/router.go
@@ -8,12 +8,19 @@ import (
 
 func newRouter(contr controller) http.Handler {
 	router := httprouter.New()
+	// router.POST("/api/albums", contr.handleAlbum())
 	router.POST("/api/albums/", contr.handleAlbum())
+	// router.GET("/api/albums/:album/status", contr.handleStatus())
 	router.GET("/api/albums/:album/status/", contr.handleStatus())
+	// router.GET("/api/albums/:album/pair", contr.handlePair())
 	router.GET("/api/albums/:album/pair/", contr.handlePair())
+	// router.GET("/api/images/:token", contr.handleImage())
 	router.GET("/api/images/:token/", contr.handleImage())
+	// router.PATCH("/api/albums/:album/vote", contr.handleVote())
 	router.PATCH("/api/albums/:album/vote/", contr.handleVote())
+	// router.GET("/api/albums/:album/top", contr.handleTop())
 	router.GET("/api/albums/:album/top/", contr.handleTop())
+	// router.GET("/api/health", contr.handleHealth())
 	router.GET("/api/health/", contr.handleHealth())
 	return router
 }

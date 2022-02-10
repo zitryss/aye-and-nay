b801d27940ac69b938ab731b5e7169d553cf8ceb
Eugene Russkikh <z8a7hp@gmail.com>
2022-02-10 22:37:34 +0100

    Apply testing suits

--- infrastructure/storage/minio_integration_test.go
+++ infrastructure/storage/minio_integration_test.go
@@ -9,58 +9,88 @@ import (
 	minioS3 "github.com/minio/minio-go/v7"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 
+	"github.com/zitryss/aye-and-nay/domain/domain"
 	. "github.com/zitryss/aye-and-nay/internal/generator"
 	. "github.com/zitryss/aye-and-nay/internal/testing"
 )
 
-func TestMinio(t *testing.T) {
-	t.Run("", func(t *testing.T) {
+func TestExampleTestSuite(t *testing.T) {
+	suite.Run(t, &MinioTestSuite{})
+}
+
+type MinioTestSuite struct {
+	suite.Suite
+	ctx     context.Context
+	cancel  context.CancelFunc
+	storage domain.Storager
+}
+
+func (suite *MinioTestSuite) SetupSuite() {
+	ctx, cancel := context.WithCancel(context.Background())
+	minio, err := NewMinio(ctx, DefaultMinioConfig)
+	require.NoError(suite.T(), err)
+	suite.ctx = ctx
+	suite.cancel = cancel
+	suite.storage = minio
+}
+
+func (suite *MinioTestSuite) SetupTest() {
+	err := suite.storage.(*Minio).Reset()
+	require.NoError(suite.T(), err)
+}
+
+func (suite *MinioTestSuite) TestMinio() {
+	suite.T().Run("", func(t *testing.T) {
 		id, ids := GenId()
 		album := id()
 		image := id()
-		minio, err := NewMinio(context.Background(), DefaultMinioConfig)
-		require.NoError(t, err)
-		f, err := minio.Get(context.Background(), album, image)
+		f, err := suite.storage.Get(suite.ctx, album, image)
 		e := minioS3.ErrorResponse{}
 		assert.ErrorAs(t, err, &e)
 		assert.Nil(t, f.Reader)
-		src, err := minio.Put(context.Background(), album, image, Png())
+		src, err := suite.storage.Put(suite.ctx, album, image, Png())
 		assert.NoError(t, err)
 		assert.Equal(t, "/aye-and-nay/albums/"+ids.Base64(0)+"/images/"+ids.Base64(1), src)
-		f, err = minio.Get(context.Background(), album, image)
+		f, err = suite.storage.Get(suite.ctx, album, image)
 		assert.NoError(t, err)
 		AssertEqualFile(t, f, Png())
-		err = minio.Remove(context.Background(), album, image)
+		err = suite.storage.Remove(suite.ctx, album, image)
 		assert.NoError(t, err)
-		f, err = minio.Get(context.Background(), album, image)
+		f, err = suite.storage.Get(suite.ctx, album, image)
 		e = minioS3.ErrorResponse{}
 		assert.ErrorAs(t, err, &e)
 		assert.Nil(t, f.Reader)
 	})
-	t.Run("", func(t *testing.T) {
+	suite.T().Run("", func(t *testing.T) {
 		id, ids := GenId()
 		album := id()
 		image := id()
-		minio, err := NewMinio(context.Background(), DefaultMinioConfig)
-		require.NoError(t, err)
-		src, err := minio.Put(context.Background(), album, image, Png())
+		src, err := suite.storage.Put(suite.ctx, album, image, Png())
 		assert.NoError(t, err)
 		assert.Equal(t, "/aye-and-nay/albums/"+ids.Base64(0)+"/images/"+ids.Base64(1), src)
-		f, err := minio.Get(context.Background(), album, image)
+		f, err := suite.storage.Get(suite.ctx, album, image)
 		assert.NoError(t, err)
 		AssertEqualFile(t, f, Png())
-		err = minio.Remove(context.Background(), album, image)
+		err = suite.storage.Remove(suite.ctx, album, image)
 		assert.NoError(t, err)
-		src, err = minio.Put(context.Background(), album, image, Png())
+		src, err = suite.storage.Put(suite.ctx, album, image, Png())
 		assert.NoError(t, err)
 		assert.Equal(t, "/aye-and-nay/albums/"+ids.Base64(0)+"/images/"+ids.Base64(1), src)
 	})
 }
 
-func TestMinioHealth(t *testing.T) {
-	minio, err := NewMinio(context.Background(), DefaultMinioConfig)
-	require.NoError(t, err)
-	_, err = minio.Health(context.Background())
-	assert.NoError(t, err)
+func (suite *MinioTestSuite) TestMinioHealth() {
+	_, err := suite.storage.Health(suite.ctx)
+	assert.NoError(suite.T(), err)
+}
+
+func (suite *MinioTestSuite) TearDownTest() {
+}
+
+func (suite *MinioTestSuite) TearDownSuite() {
+	err := suite.storage.(*Minio).Reset()
+	require.NoError(suite.T(), err)
+	suite.cancel()
 }

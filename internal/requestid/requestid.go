04f125b92c3194c9e91de317d4bec3858b63404d
Eugene Russkikh <z8a7hp@gmail.com>
2022-07-19 10:39:01 +0200

    Check for the nil context

--- internal/requestid/requestid.go
+++ internal/requestid/requestid.go
@@ -17,10 +17,16 @@ var (
 )
 
 func Set(ctx context.Context) context.Context {
+	if ctx == nil {
+		ctx = context.Background()
+	}
 	return context.WithValue(ctx, ctxRequestId, requestID.Inc())
 }
 
 func Get(ctx context.Context) uint64 {
+	if ctx == nil {
+		return 0
+	}
 	reqId, ok := ctx.Value(ctxRequestId).(uint64)
 	if !ok {
 		return 0
